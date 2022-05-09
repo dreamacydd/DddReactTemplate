72330ad8805ce4ea834b87805bd3424ee8984b75
William Ellison <waellison@gmail.com>
2022-05-09 16:55:48 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.less$/,
+                use: ["style-loader", "less-loader"]
+            }
+        ]
+    },
+    plugins: [htmlPlugin],
+    resolve: {
+        extensions: [".tsx", ".jsx", ".ts", ".js"]
+    }
+};
